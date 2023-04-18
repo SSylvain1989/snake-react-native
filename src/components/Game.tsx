a99a03fafb54e08fc3c5df52256b03ee04ae2d89
SSylvain1989 <sylvain.sigonnez@gmail.com>
2023-04-18 12:05:51 +0200

    feat: detect gesture on mobile

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,14 +1,44 @@
 import * as React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
 import { Colors } from "../styles/colors";
+import { GestureEventType } from "../types/types";
+
+const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
+const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
+const GAME_BOUNDS = { xMin: 0, xMax: 35, yMin: 0, yMax: 63 };
+const MOVE_INTERVAL = 50;
+const SCORE_INCREMENT = 10;
 
 export default function Game(): JSX.Element {
-	return <SafeAreaView style={styles.container}></SafeAreaView>;
+	const handleGesture = (event: GestureEventType) => {
+		const { translationX, translationY } = event.nativeEvent;
+		if (Math.abs(translationX) > Math.abs(translationY)) {
+			if (translationX > 0) {
+				// moving right 👉
+			} else {
+				// moving left 👈
+			}
+		} else {
+			if (translationY > 0) {
+				// moving down 👇
+			} else {
+				// moving up 👆
+			}
+		}
+		console.log(translationX, translationY);
+	};
+
+	return (
+		<PanGestureHandler onGestureEvent={handleGesture}>
+			<SafeAreaView style={styles.container}></SafeAreaView>
+		</PanGestureHandler>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+	container: {
+		flex: 1,
 		backgroundColor: Colors.primary,
 	},
 });
