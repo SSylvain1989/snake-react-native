53caf26781a8ca575143692c7e2e3bf46c84ace3
SSylvain1989 <sylvain.sigonnez@gmail.com>
2023-04-19 10:22:34 +0200

    feat: add snake face

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,9 @@
-import * as React from "react";
-import { SafeAreaView, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { Colors } from "../styles/colors";
-import { GestureEventType } from "../types/types";
+import { Coordinate, Direction, GestureEventType } from "../types/types";
+import Snake from "./Snake";
 
 const SNAKE_INITIAL_POSITION = [{ x: 5, y: 5 }];
 const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
@@ -11,19 +12,29 @@ const MOVE_INTERVAL = 50;
 const SCORE_INCREMENT = 10;
 
 export default function Game(): JSX.Element {
+	const [direction, setDirection] = useState<Direction>(Direction.Right);
+	const [snake, setSnake] = useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
+	const [food, setFood] = useState<Coordinate>(FOOD_INITIAL_POSITION);
+	const [isGameOver, seIsGameOver] = useState<boolean>(false);
+	const [paused, setPaused] = useState<boolean>(false);
+
 	const handleGesture = (event: GestureEventType) => {
 		const { translationX, translationY } = event.nativeEvent;
 		if (Math.abs(translationX) > Math.abs(translationY)) {
 			if (translationX > 0) {
 				// moving right 👉
+				setDirection(Direction.Right);
 			} else {
 				// moving left 👈
+				setDirection(Direction.Left);
 			}
 		} else {
 			if (translationY > 0) {
 				// moving down 👇
+				setDirection(Direction.Down);
 			} else {
 				// moving up 👆
+				setDirection(Direction.Up);
 			}
 		}
 		console.log(translationX, translationY);
@@ -31,7 +42,11 @@ export default function Game(): JSX.Element {
 
 	return (
 		<PanGestureHandler onGestureEvent={handleGesture}>
-			<SafeAreaView style={styles.container}></SafeAreaView>
+			<SafeAreaView style={styles.container}>
+				<View style={styles.boundaries}>
+					<Snake snake={snake} />
+				</View>
+			</SafeAreaView>
 		</PanGestureHandler>
 	);
 }
@@ -41,4 +56,12 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: Colors.primary,
 	},
+	boundaries: {
+		flex: 1,
+		borderColor: Colors.primary,
+		borderWidth: 12,
+		borderBottomLeftRadius: 30,
+		borderBottomRightRadius: 30,
+		backgroundColor: Colors.background,
+	}
 });
