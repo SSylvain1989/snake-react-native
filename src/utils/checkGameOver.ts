37b7ad325b238418df228e5e7ab071f88fd5cb30
SSylvain1989 <sylvain.sigonnez@gmail.com>
2023-05-11 10:13:20 +0200

    feat: add logic to avoid food on wall and snake

--- src/utils/checkGameOver.ts
+++ src/utils/checkGameOver.ts
@@ -1,31 +1,3 @@
-// import { Coordinate, GameBounds } from "../types/types";
-
-// export const checkGameOver = (
-// 	snakeHead: Coordinate,
-// 	boundaries: GameBounds,
-// 	wall: Coordinate[],
-// 	wallBis: Coordinate[]
-// ): boolean => {
-// 	// TODO : function to not repeat myself here
-
-// 	function snakeTouchingWall (wall: Coordinate[], snakeHead: Coordinate) {
-// 		return wall.some((wallPiece) => {
-// 		return wallPiece.x === snakeHead.x && wallPiece.y === snakeHead.y;
-// 	});
-// 	}
-// const isTouchingWall = snakeTouchingWall(wall, snakeHead);
-// const isTouchingWallBis = snakeTouchingWall(wallBis, snakeHead);
-
-// 	return (
-// 		snakeHead.x > boundaries.xMax ||
-// 		snakeHead.x < boundaries.xMin ||
-// 		snakeHead.y > boundaries.yMax ||
-// 		snakeHead.y < boundaries.yMin ||
-// 		isTouchingWall ||
-// 		isTouchingWallBis
-// 	);
-// };
-
 import { Coordinate, GameBounds } from "../types/types";
 
 export const checkGameOver = (
