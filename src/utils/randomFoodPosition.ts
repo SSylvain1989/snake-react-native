3826a992835582271f753639d17d5e5e802a6ecf
SSylvain1989 <sylvain.sigonnez@gmail.com>
2023-06-05 09:36:48 +0200

    chore: minor change

--- src/utils/randomFoodPosition.ts
+++ src/utils/randomFoodPosition.ts
@@ -4,7 +4,7 @@ export const randomFoodPosition = (
 	maxX: number,
 	maxY: number,
 	allWalls: Coordinate[][],
-	snakePosition : Coordinate[]
+	snakePosition: Coordinate[]
 ) => {
 	while (true) {
 		const randomFoodPosition = {
@@ -25,21 +25,29 @@ function isTouchingSnake(
 	snakePosition: Coordinate[]
 ): boolean {
 	for (const segment of snakePosition) {
-		if (segment.x === randomFoodPosition.x && segment.y === randomFoodPosition.y) {
+		if (
+			segment.x === randomFoodPosition.x &&
+			segment.y === randomFoodPosition.y
+		) {
 			return true;
 		}
 	}
 	return false;
 }
 
-function isTouchingWall(randomFoodPosition: Coordinate, walls: Coordinate[][]): boolean {
+function isTouchingWall(
+	randomFoodPosition: Coordinate,
+	walls: Coordinate[][]
+): boolean {
 	for (const wall of walls) {
 		for (const coordinate of wall) {
-			if (randomFoodPosition.x === coordinate.x && randomFoodPosition.y === coordinate.y) {
+			if (
+				randomFoodPosition.x === coordinate.x &&
+				randomFoodPosition.y === coordinate.y
+			) {
 				return true;
 			}
 		}
 	}
 	return false;
 }
-
